b4af8e54136664d5f451cacd06a2436237c0a6ad
Augustinas <augustinas.jukna@gmail.com>
2023-10-26 07:58:45 +0300

    Update GeneController.java

--- backend/src/main/java/edu/ktu/GenomeLab/controllers/GeneController.java
+++ backend/src/main/java/edu/ktu/GenomeLab/controllers/GeneController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path="/genomes")
+@RequestMapping(path="/genes")
 public class GeneController {
     @Autowired
     private GeneRepository geneRepository;
